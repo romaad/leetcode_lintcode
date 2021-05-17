a2239ce9cb495d172864802afe7964e0e980b461
romaad <koko2405000@gmail.com>
2021-05-18 00:11:49 +0100

    Create 13 · Implement strStr().cpp

---
+++ lint/13 · Implement strStr().cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
+     */
+    int strStr(string &source, string &target) {
+        // Write your code here
+        //#sorry not sorry
+        return source.find(target);
+    }
+};
